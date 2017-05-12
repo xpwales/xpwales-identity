53e8ee6c9cc2cf39fee3ed68135cc0b902f30243
Michael Adrian <michael.adrian@xpwales.com>
2017-05-12 19:44:08 +0100

    Add 'single integer value' identity class

---
+++ src/Identity/SingleIntegerIdentity.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Xpwales\Identity\Identity;
+
+use Xpwales\Identity\Domainspace\DomainspaceTrait;
+
+class SingleIntegerIdentity implements IdentityInterface
+{
+	use DomainspaceTrait;
+
+	/**
+	 * @var int
+	 */
+	private $idValue = null;
+
+	/**
+	 * @inheritdoc
+	 *
+	 * @see IdentityInterface
+	 */
+	public function setValue($value, $name=null)
+	{
+		$value = (int) $value;
+
+		if (empty($value) === true) {
+			$msg = 'Value cannot be set with empty value';
+			throw new Exception\InvalidArgumentException($msg);
+		}
+
+		$this->idValue = $value;
+
+		return $this;
+	}
+
+	/**
+	 * @inheritdoc
+	 *
+	 * @see IdentityInterface
+	 */
+	public function getValues()
+	{
+		if (null === $this->idValue) {
+			$msg = 'Value must be set before access';
+			throw new Exception\RuntimeException($msg);
+		}
+
+		$values = [$this->idValue];
+
+		return $values;
+	}
+
+	/**
+	 * @inheritdoc
+	 *
+	 * @see IdentityInterface
+	 */
+	public function isComplete()
+	{
+		$isComplete = !is_null($this->idValue);
+
+		return $isComplete;
+	}
+
+}//end class
